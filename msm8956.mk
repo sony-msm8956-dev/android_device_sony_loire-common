7a5d890d7eafacd1b6adf550013cd91e937d9dd1
Mikhail Naganov <mnaganov@google.com>
2018-01-08 17:05:28 -0800

    msm8956-common: Uprev soundtrigger to 2.1
    Bug: 68823037
    Change-Id: If634345594ea6d574f49d58e29b48165c3b9458b
    Test: "ok google" and music iq from locked screen

--- msm8956.mk
+++ msm8956.mk
@@ -30,7 +30,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.primary.msm8952 \
     audio.a2dp.default \
     audio.r_submix.default \
