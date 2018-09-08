ccbe1047fde559d915d24190586934e90601fd55
Chippa-a <vusal1372@gmail.com>
2018-09-08 21:23:51 +0300

    loire: Add support for dual SIM variant
    Change-Id: I83bdde4d935386852246b82944d0ce5bab68b046

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,6 +43,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.radio.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.radio.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
