f80fe2ecaf345917ff00138d56a4b3167a015e5d
Chippa-a <vusal1372@gmail.com>
2018-10-22 21:48:39 +0300

    loire: Switch to OSS thermanager
    Change-Id: Ie51684eacfc8e5cb3e7da0b6539217a32eac9b5d

--- platform.mk
+++ platform.mk
@@ -281,8 +281,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    thermanager
 
 # USB
 PRODUCT_PACKAGES += \
