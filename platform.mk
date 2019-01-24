09cd8826454bb251c6bcfc3d37eb440d3332432e
Chippa-a <vusal1372@gmail.com>
2019-01-25 00:15:32 +0300

    loire: camera: Add and use Camera Wrapper
    Change-Id: I83ed4ab1365e1642959300585933eddbe72bcd1c

--- platform.mk
+++ platform.mk
@@ -98,6 +98,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    camera.qcom \
     Snap
 
 # Charger
