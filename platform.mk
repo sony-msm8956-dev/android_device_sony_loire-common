a85cf9b053a36c7d8004dd36cd339e74c9c00b6e
Chippa-a <vusal1372@gmail.com>
2019-03-03 21:56:36 +0300

    loire: Import health HAL 2.0 implementation
    Based on bonito health HAL.
    
    Change-Id: I73d1159fded97332b758c0fbf4e258bcfa8c3f39
    Signed-off-by: Chippa-a <vusal1372@gmail.com>

--- platform.mk
+++ platform.mk
@@ -154,8 +154,7 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service.loire
 
 # HIDL
 PRODUCT_PACKAGES += \
