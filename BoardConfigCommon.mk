6eaf8bafeffa890812b04f260d85abdc4c70d7dc
Chippa-a <vusal1372@gmail.com>
2020-01-14 21:43:05 +0300

    loire: Switch to full dexpreopt builds
    We have more than enough space on system to
    handle this.
    
    Change-Id: If732005d762a2d13a531b7dfd6d0aab427a56824

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,10 +170,10 @@ TARGET_USES_INTERACTION_BOOST := true
 # Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
+    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Root
 BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware idd persist rca
