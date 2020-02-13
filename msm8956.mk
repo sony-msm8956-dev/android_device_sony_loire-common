4b267ba72dfe65ee388afacfbd5a817661df4de3
Bharath Gopal <gopalb@codeaurora.org>
2020-02-14 00:12:22 +0530

    msm8956-common: Enable graphics mapper version 2.1
    Change-Id: I4be4782e079fec6762d012b3b07a2ea565d86efe

--- msm8956.mk
+++ msm8956.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     copybit.msm8952 \
     gralloc.msm8952 \
     hwcomposer.msm8952 \
