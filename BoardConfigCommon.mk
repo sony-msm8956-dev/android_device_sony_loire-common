53040542cef778c6f82698b1437e92fac23490ee
dianlujitao <dianlujitao@lineageos.org>
2022-01-17 15:53:26 +0100

    loire: Remove custom kernel cross compile prefix
    Change-Id: Icc58f38fa2f66b833c73902d6733f84b77dcb575

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,6 @@ TARGET_NO_BOOTLOADER := true
 # Kernel properties
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/sony/msm8956
 
 # Kernel configurations
