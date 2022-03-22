652bf5bcc50b07b2c28d51b07b98e424785e6615
Alessandro Astone <ales.astone@gmail.com>
2022-03-22 18:56:43 +0000

    loire: Move camera provider HAL to passthrough
     * Camera devices were getting discovered WAY too late,
       after android had dispatched the BOOT_COMPLETED broadcast,
       which made Camera2 mark itself as disabled.
    
    Change-Id: I1a282f99a98330ad1a8dadded9e69734136d7469

--- platform.mk
+++ platform.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.qcom \
     Snap
 
