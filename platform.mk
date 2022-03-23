b105ca8ea71b1ed9b91ec75f403bcca0d1994fc0
Roshan Pius <rpius@google.com>
2022-03-23 22:15:31 +0100

    loire: wifi: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- platform.mk
+++ platform.mk
@@ -330,6 +330,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
+    WifiOverlay \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
