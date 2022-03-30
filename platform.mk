35dfbe102d00b093038d42a30d894562eb5e2381
Alessandro Astone <ales.astone@gmail.com>
2022-03-30 15:48:37 +0200

    loire: Migrate to perf@2.0 service.
    * Update blobs.
    * Import and adapt targetconfig / targetresourceconfigs.
    * Uprev hal in manifest.
    
    Change-Id: Id0a21de94a3da7ec6a9164f33717c507b46b1cd5

--- platform.mk
+++ platform.mk
@@ -251,7 +251,8 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@1.0
+    vendor.qti.hardware.perf@1.0 \
+    vendor.qti.hardware.perf@2.0
 
 # Prebuilt Protobuf
 PRODUCT_COPY_FILES += \
