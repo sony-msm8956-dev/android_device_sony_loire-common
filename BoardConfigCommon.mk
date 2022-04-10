2ecdde6f29eb1ad3428dca2f9ce856dc07943294
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2022-04-10 13:04:44 +0200

    msm8956-common: Import WiFi flags for Broadcom chip
    Change-Id: I100091085f9a7047dfb781c8415937ced9d9ae30

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,3 +130,15 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
 # Seccomp filters
 BOARD_SECCOMP_POLICY := $(COMMON_PATH)/seccomp
+
+
+# Wi-Fi
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
