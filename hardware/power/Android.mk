e57c8201c1aa6a6dd52af4d6130a4b1963874ad0
jmpfbmx <jmpf.bmx@gmail.com>
2022-04-10 16:35:11 +0200

    msm8956-common: power: Switch to TARGET_BOARD_PLATFORM
     * Is useless building 1 power hal per device when we just can build 1 per platform
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ief20e872d5fc5249684ca2a5de33247866e1c5bc

--- hardware/power/Android.mk
+++ hardware/power/Android.mk
@@ -26,7 +26,7 @@ LOCAL_C_INCLUDES := external/expat/lib
 
 LOCAL_SRC_FILES := power.c rqbalance_halext.c expatparser.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils libexpat
-LOCAL_MODULE := power.$(TARGET_DEVICE)
+LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_OWNER := sony
