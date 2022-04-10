2ea3f44258f590fed046e12ce48d28fc5dfe5480
Badhri Jagan Sridharan <Badhri@google.com>
2022-04-10 21:08:16 +0200

    msm8956-common: Build binderized USB HIDL HALs
    Change-Id: Ibd13a20ec5badfa8f71223111c5878a45e74161f

--- msm8956.mk
+++ msm8956.mk
@@ -318,6 +318,11 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Usb HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0 \
+    android.hardware.usb@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     hostapd \
