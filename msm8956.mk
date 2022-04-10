0f49360adb397eef1a3f835ea909040e74700310
Chia-I Wu <olv@google.com>
2022-04-10 19:55:42 +0200

    msm8956-common: Build binderized Graphics HIDL HAL
    Change-Id: Iece5309882289c0d5a106e54c82661480851a3d9

--- msm8956.mk
+++ msm8956.mk
@@ -90,6 +90,11 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
     copybit.msm8952 \
     gralloc.msm8952 \
     hwcomposer.msm8952 \
