a750b1bbe2e26ede58ec2dda4afc949bd24564e8
Louis Popi <theh2o64@gmail.com>
2022-04-10 14:39:40 +0200

    msm8956-common: Build libwpa_client
    Change-Id: If6ad7d8ba83b7e72960750dd8d190e0b6fa07a82

--- msm8956.mk
+++ msm8956.mk
@@ -201,7 +201,8 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    hostapd
+    hostapd \
+    libwpa_client
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
