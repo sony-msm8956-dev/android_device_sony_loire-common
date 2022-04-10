161df259ed09bad1a82864d21b5f3ab8cbe7200b
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2022-04-10 20:31:41 +0200

    msm8956-common: add light HAL for Android 8
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>
    Change-Id: I05a721205612d14d811bdd58eefe182908ad893a

--- msm8956.mk
+++ msm8956.mk
@@ -132,6 +132,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl
+
 # Media
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
