3e6dc289a96b9a09ab705272d48b8ff2fcd7ae4c
Prashant Malani <pmalani@google.com>
2022-04-10 21:09:37 +0200

    msm8956-common: Build binderized Vibrator HIDL HAL
    Change-Id: I88877df3e9a344c69aea25b49041c7a62a613db4

--- msm8956.mk
+++ msm8956.mk
@@ -323,6 +323,11 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0 \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     hostapd \
