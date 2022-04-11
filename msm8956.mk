da93313a42f74a794a82175b4a57bf09fc7207dd
Alin Jerpelea <alin.jerpelea@sony.com>
2022-04-11 07:53:28 +0200

    msm8956-common: Build camera.msm8952
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>
    Change-Id: I48973e4fd002036730b5a8df98dd40e3f58326c8

--- msm8956.mk
+++ msm8956.mk
@@ -62,7 +62,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    camera.msm8952
 
 # Configstore
 PRODUCT_PACKAGES += \
