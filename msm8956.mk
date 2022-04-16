6f0876204b79265e129acb253eee1b3bbbe5388d
Hung-ying Tyan <tyanh@google.com>
2022-04-16 10:15:18 +0200

    msm8956-common: Uprev ConfigStore to 1.1
    Bug: 69691076
    Test: built and tested on sailfish
    Change-Id: Ieb4b329ccf2f56e304cd2b62ba12183e514aaff1

--- msm8956.mk
+++ msm8956.mk
@@ -72,8 +72,8 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-impl \
-    android.hardware.configstore@1.0-service
+    android.hardware.configstore@1.1-impl \
+    android.hardware.configstore@1.1-service
 
 # For config.fs
 PRODUCT_PACKAGES += \
