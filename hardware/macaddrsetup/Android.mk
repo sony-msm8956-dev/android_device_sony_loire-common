13219bc493015c4795af86755499512c83deb8c6
jmpfbmx <jmpf.bmx@gmail.com>
2022-06-04 15:43:52 +0200

    msm8956-common: macaddrsetup: Drop unused sdk25 rc
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ib0abe3825ae45b7db5460bdbe97797d88f31c73f

--- hardware/macaddrsetup/Android.mk
+++ hardware/macaddrsetup/Android.mk
@@ -22,18 +22,9 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils
 
 LOCAL_MODULE := macaddrsetup
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 29),)
 LOCAL_MODULE_OWNER := sony
 LOCAL_INIT_RC      := macaddrsetup.rc
 LOCAL_PROPRIETARY_MODULE := true
-else
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 25),)
-LOCAL_MODULE_OWNER := sony
-LOCAL_INIT_RC      := macaddrsetup_sdk25.rc
-LOCAL_PROPRIETARY_MODULE := true
-endif
-endif
-
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
