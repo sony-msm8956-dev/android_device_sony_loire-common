b3d511e92e17763eb44b6a9b0750726caeeec343
Brijesh Patel <pbrijesh@codeaurora.org>
2022-08-12 11:58:20 +0200

    msm8956-common: hardware: media: mm-core: Generate omx core header library.
    CRs-Fixed: 2596430
    
    Change-Id: If5dab1762e30486193a221d8b604193cb989c3e2

--- hardware/qcom/media/msm8998/mm-core/Android.mk
+++ hardware/qcom/media/msm8998/mm-core/Android.mk
@@ -100,7 +100,12 @@ LOCAL_COPY_HEADERS      += inc/QCMetaData.h
 #===============================================================================
 
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/src/common
-LOCAL_C_INCLUDES        += $(LOCAL_PATH)/inc
+
+LOCAL_HEADER_LIBRARIES := \
+        libomxcore_headers
+
+LOCAL_EXPORT_HEADER_LIBRARY_HEADERS := libomxcore_headers
+
 LOCAL_PRELINK_MODULE    := false
 LOCAL_MODULE            := libOmxCore
 LOCAL_MODULE_TAGS       := optional
@@ -129,7 +134,12 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/src/common
-LOCAL_C_INCLUDES        += $(LOCAL_PATH)/inc
+
+LOCAL_HEADER_LIBRARIES := \
+        libomxcore_headers
+
+LOCAL_EXPORT_HEADER_LIBRARY_HEADERS := libomxcore_headers
+
 LOCAL_PRELINK_MODULE    := false
 LOCAL_MODULE            := libmm-omxcore
 LOCAL_MODULE_TAGS       := optional
@@ -150,5 +160,13 @@ LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/qc_registry_table.c
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libomxcore_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_HEADER_LIBRARY)
 
 endif #BUILD_TINY_ANDROID
