52c0d380f97f666dd1926f9512dfd9d44268f76a
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2022-08-12 11:56:34 +0200

    msm8956-common: Exclude libqservice from binder interface whitelisting
    - Needed after: https://github.com/LineageOS/android_build_soong/commit/d094d4006fc2352af55fc1e16fbcdbbd2a721c7f
    
    Change-Id: Iaf56cc23d578ad6ac450e7dcf2ff91c1bcc2cf4f

--- hardware/qcom/display/msm8998/libqservice/Android.mk
+++ hardware/qcom/display/msm8998/libqservice/Android.mk
@@ -7,7 +7,7 @@ LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libbinder
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdqservice\" -Wno-sign-conversion
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdqservice\" -Wno-sign-conversion -DDO_NOT_CHECK_MANUAL_BINDER_INTERFACES
 LOCAL_CFLAGS                  += -Wno-error
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := QService.cpp \
