be617bff588a6ed6bae30d656ab2035a6ce59318
Albert I <kras@raphielgang.org>
2022-08-13 19:00:21 +0200

    msm8956-common: Makefile: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- msm8956.mk
+++ msm8956.mk
@@ -142,6 +142,10 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/init/android.hardware.wifi@1.0-service.legacy.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.wifi@1.0-service.legacy.rc
